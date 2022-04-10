35798d917f8f1a94907ba215ce61eef46be5c56f
mabus <m.buscaroli@outlook.com>
2022-04-10 11:28:12 +0100

    first commit, basic tailwindcss conf

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      light1: '#fefae0',
+      light2: '#faedcd',
+      dark: '#d4a373',
+      accent1: '#e9edc9',
+      accent2: '#ccd5ae',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
