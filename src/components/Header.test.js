dc2842bfbbfdfb8626ad480d3dc32a61acfc27f1
mabus <m.buscaroli@outlook.com>
2022-04-10 23:00:28 +0100

    refactored the tests for the header, they are now included within several "describe" globals and are tidier

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,65 +2,71 @@ import { screen, render } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import Header from './Header'
 
-test('the heading is displayed', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+describe('Heading', () => {
+  test('is displayed', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
 
-  const header = screen.getByRole('heading', { level: 1 })
-  expect(header).toBeInTheDocument()
+    const header = screen.getByRole('heading', { level: 1 })
+    expect(header).toBeInTheDocument()
+  })
+
+  test('has a class of grow-0', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+
+    const header = screen.getByRole('heading', { level: 1 })
+    expect(header).toHaveClass('text-5xl')
+  })
+
+  test('has the expected title', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+
+    const header = screen.getByRole('heading', { level: 1 })
+    expect(header).toHaveTextContent('TailwindCSS and TDD')
+  })
 })
 
-test('the heading has a class of grow-0', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+describe('The theme', () => {
+  test('has the sun icon displayed', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
 
-  const header = screen.getByRole('heading', { level: 1 })
-  expect(header).toHaveClass('text-5xl')
+    const icon = screen.getByTestId('sunicon')
+    expect(icon).toBeInTheDocument()
+  })
+
+  test('has the moon icon displayed after the icon is clicked once', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+
+    const clickableContainer = screen.getByTestId('header-theme')
+    userEvent.click(clickableContainer)
+
+    const icon = screen.getByTestId('moonicon')
+    expect(icon).toBeInTheDocument()
+  })
+
+  test('has the sun icon displayed after the icon is clicked twice', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+
+    const clickableContainer = screen.getByTestId('header-theme')
+    userEvent.click(clickableContainer)
+    userEvent.click(clickableContainer)
+
+    const icon = screen.getByTestId('sunicon')
+    expect(icon).toBeInTheDocument()
+  })
 })
 
-test('the heading has the expected title', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+describe('The bitcoin element', () => {
+  test('is displayed', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
 
-  const header = screen.getByRole('heading', { level: 1 })
-  expect(header).toHaveTextContent('TailwindCSS and TDD')
-})
+    const bitcoin = screen.getByTestId('header-bitcoin')
+    expect(bitcoin).toBeInTheDocument()
+  })
 
-test('the themes sun icon is displayed', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
+  test('has a price of 32000', () => {
+    render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
 
-  const icon = screen.getByTestId('sunicon')
-  expect(icon).toBeInTheDocument()
-})
-
-test('the themes moon icon is displayed after icon is clicked once', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
-
-  const clickableContainer = screen.getByTestId('header-theme')
-  userEvent.click(clickableContainer)
-
-  const icon = screen.getByTestId('moonicon')
-  expect(icon).toBeInTheDocument()
-})
-
-test('the themes sun icon is displayed after icon is clicked twice', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
-
-  const clickableContainer = screen.getByTestId('header-theme')
-  userEvent.click(clickableContainer)
-  userEvent.click(clickableContainer)
-
-  const icon = screen.getByTestId('sunicon')
-  expect(icon).toBeInTheDocument()
-})
-
-test('the element that shows the bitcoin price is displayed', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
-
-  const bitcoin = screen.getByTestId('header-bitcoin')
-  expect(bitcoin).toBeInTheDocument()
-})
-
-test('the element that shows the bitcoin price has a price of 32000', () => {
-  render(<Header title="TailwindCSS and TDD" bitcoin="32000" />)
-
-  const bitcoin = screen.getByTestId('header-bitcoin')
-  expect(bitcoin).toHaveTextContent('32000')
+    const bitcoin = screen.getByTestId('header-bitcoin')
+    expect(bitcoin).toHaveTextContent('32000')
+  })
 })
